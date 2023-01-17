e92a374a300a090f16436fdf049bf1db402c4201
petroyank <vandeom@icloud.com>
2023-01-17 22:41:58 +0100

    added achievement model

---
+++ src/interfaces/statsfm/achievement.ts
@@ -0,0 +1,19 @@
+enum AchievementCategory {
+  'STREAK',
+
+  'ONE-TIME',
+
+  'CUSTOM'
+}
+
+export interface Achievement extends Object {
+  image: string;
+  category: AchievementCategory;
+  userId: string;
+  title: string;
+  description: string;
+  metadata: JSON;
+  active: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
