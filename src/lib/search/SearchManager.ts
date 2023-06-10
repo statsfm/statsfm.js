775b1d73ee1ee06ad9f547a52af3ef63a2792f4f
Stijn van der Kolk <stijn@kolkies.dev>
2023-06-10 17:37:55 +0200

    feat(search): add elastic search route

--- src/lib/search/SearchManager.ts
+++ src/lib/search/SearchManager.ts
@@ -20,4 +20,22 @@ export default class SearchManager extends Manager {
     // @ts-expect-error needs to be items but type is no array
     return res.data.items;
   }
+
+  async searchElastic(
+    query: string,
+    type: statsfm.SearchTypes[],
+    options: QueryWithPaging = {}
+  ): Promise<statsfm.SearchResults> {
+    // TODO: implement paging
+    const res = await this.http.get<statsfm.SearchResults>(`/search/elastic`, {
+      query: {
+        query,
+        type: type.join(','),
+        ...options
+      }
+    });
+
+    // @ts-expect-error needs to be items but type is no array
+    return res.data.items;
+  }
 }
